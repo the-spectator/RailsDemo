70718baec0e6df05c49809ddc9cc248e2504a862
the-spectator <mailakshayb1@gmail.com>
2018-01-23 14:09:23 +0530

    Created model for user

---
+++ db/migrate/20180123083812_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :gender
+      t.date :dob
+      t.string :mobile_number
+      t.string :email
+      t.string :passoword
+
+      t.timestamps
+    end
+  end
+end

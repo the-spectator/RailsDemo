c3d37137da4a245ad093b13565ef438ab8cbcdd2
the-spectator <mailakshayb1@gmail.com>
2018-01-25 18:18:45 +0530

    added user profile option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
-  	p "...................."
   	if @user.save
   		redirect_to root_path
   	else
@@ -22,6 +21,14 @@ class UsersController < ApplicationController
   	end
   end
 
+  def destroy
+  	@user = User.find(params[:id])
+  	redirect_to root_path if @user.destroy
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
   def user_params
   	params.require(:user).permit(:name, :last_name, :mobile_number, :password_confirmation, :email, :dob, :password, :gender)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'users/index'
-  resources :users
+  resources :users do
+    resources :addresses
+    resources :posts
+  end
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # when single resource then we don't have index as we have single resource we dont want to list it

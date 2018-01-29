ecf661f40666f9b5e6527c38b11edf2f2534b226
the-spectator <mailakshayb1@gmail.com>
2018-01-29 17:09:32 +0530

    Added api for creating and deleting user record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,40 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
-  	if @user.save
-  		redirect_to root_path
-  	else
-  	  p @user.errors.messages
-  		render 'new'
-  	end
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path}
+        format.json { render :json => User.last.to_json }
+      else
+        format.html { render 'new' }
+        format.json { render :json => @user.errors.to_json }
+      end
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      respond_to do |format|
+        format.html
+        format.json {render :json => @user.to_json}
+      end
+    end
   end
 
   def destroy
   	@user = User.find(params[:id])
-  	redirect_to root_path if @user.destroy
+    respond_to do |format|
+      if @user.destroy
+        format.html {redirect_to root_path}
+        format.json {render :json => "Deleted successfully".to_json}
+      end
+    end
+  	# redirect_to root_path if @user.destroy
   end
 
   def show
